218d9198baab347f47bf4aa5aea83fbd62da5b5d
Petr Kucera <petr@khome.cz>
2022-08-18 11:14:16 +0200

    Add files

--- Azure_DevOps_Agents_Software/Linux/run.sh
+++ Azure_DevOps_Agents_Software/Linux/run.sh
@@ -6,8 +6,22 @@ print_header() {
    echo -e "${lightcyan}$1${nocolor}"
 }
 
-
 print_header "Start with software installation"
+
+repo_owner="petrkucerak"
+repo_name="scripts-library"
+repo_path="Azure_DevOps_Agents_Software/Linux/software"
+url="https://api.github.com/repos/${repo_owner}/${repo_name}/contents/${repo_path}"
+
+response=$(curl $url | jq -C -r '.[] | (.download_url)')
+IFS=' ' read -r -a sources <<<"$response"
+
+for source in "${sources[@]}"; do
+   echo "$source"
+done
+
+# curl \
+#   https://api.github.com/repos/${OWNER}/${REPO}/contents/${PATH}
 
 cd software || exit
 
